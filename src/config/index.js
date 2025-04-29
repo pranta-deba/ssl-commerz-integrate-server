a57acf46e38992d96e32c70bb684d893904e34b0
Pranto Deb <rajprit0m@outlook.com>
2025-04-29 23:19:06 +0600

    [Config ENV]

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT || 5000,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+};
