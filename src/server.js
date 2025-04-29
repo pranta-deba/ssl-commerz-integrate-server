cac210248eaf9e2cad4f9558d300eec35d254b58
Pranto Deb <rajprit0m@outlook.com>
2025-04-29 23:31:50 +0600

    [Server]

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import config from "./config";
 
 let server;
-async function main() {}
+async function main() {
+  try {
+    await mongoose.connect(config.database_url);
+    server = app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 main();
